f0ce73bb5f4da03629a78221ca44d03ccc9ef7ec
igreactive <igreactive@gmail.com>
2012-08-29 16:47:59 +0200

    Require EconomicSoapClient

--- php/economic-ajax.php
+++ php/economic-ajax.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'EconomicSoapClient.php';
+require_once 'EconomicSoapClient.php';
 $economic_client = new EconomicSoapClient();
 $accounts = $economic_client->getAccounts();
 echo json_encode($accounts);
