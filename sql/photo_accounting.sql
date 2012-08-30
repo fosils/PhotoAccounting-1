1b32b71db3e3e3a3bbd10a5613c4bd3136760bf9
igreactive <igreactive@gmail.com>
2012-08-30 12:21:46 +0200

    Add image_name column

--- sql/photo_accounting.sql
+++ sql/photo_accounting.sql
@@ -5,7 +5,7 @@
 -- \i /home/pmg/Documents/photo_accounting/www/PhotoAccounting/sql/photo_accounting.sql
 
 DROP TABLE IF EXISTS entries;
-CREATE TABLE entries (id SERIAL PRIMARY KEY, customer_id INT, entry_id INT, image_id INT, entry_date DATE DEFAULT current_date, text VARCHAR(9999), amount NUMERIC(20, 2) DEFAULT 0.00, account INT, offset_account INT);
+CREATE TABLE entries (id SERIAL PRIMARY KEY, customer_id INT, entry_id INT, image_id INT, image_name character varying(255), entry_date DATE DEFAULT current_date, text VARCHAR(9999), amount NUMERIC(20, 2) DEFAULT 0.00, account INT, offset_account INT);
 CREATE INDEX image_id_idx ON entries (image_id);
 
 INSERT INTO entries (customer_id, entry_id, image_id, entry_date, text, amount, account, offset_account) VALUES (1,1,1,'2012-08-14','TEST 1',1,1,1);
