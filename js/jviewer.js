1ea91e7d358dd04be5fb283728b85270b2be6805
David Andersen <mrdavidandersen@gmail.com>
2012-09-17 23:21:17 +0300

    Redid earlier commit by flybot

--- js/jviewer.js
+++ js/jviewer.js
@@ -436,6 +436,7 @@ JSViewer = function () {
      */
     keyDownHandler = function (Y, total_number_images, POST_CACHE, PRE_CACHE) {
         return function (e) {
+            if(document.activeElement.id == "jsv_tekst") return;
             e.preventDefault();
 
             switch (e.keyCode) {
@@ -461,6 +462,7 @@ JSViewer = function () {
      */
     keyUpHandler = function (Y, total_number_images, POST_CACHE, PRE_CACHE) {
         return function (e) {
+            if(document.activeElement.id == "jsv_tekst") return;
             e.preventDefault();
 
             var valas = my_key_codes[String.fromCharCode(e.keyCode).toLowerCase()];
