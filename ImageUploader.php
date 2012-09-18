e2eed7ecb74a0ebe45ce3504422408dff845dc01
Nuked Software Systems <nukedsoftware@gmail.com>
2012-09-18 06:32:53 -0500

    Added AmazonS3 service to the Email.php and ImageUploader.php

--- ImageUploader.php
+++ ImageUploader.php
@@ -1,8 +1,8 @@
 <?php
 
-/** COPYRIGHT Time at Task Aps*/
-
-
+	/** COPYRIGHT Time at Task Aps*/
+	require_once "aws-sdk/1.5.14/sdk.class.php";
+	
 	$xmlContent = array();
 	if(sizeof($_FILES) > 0) {
 		foreach($_FILES as $imageFile) {
@@ -10,6 +10,13 @@
 				strlen($imageFile['tmp_name']) == 0) {
 				echo "<response><code>200</code></response>";
 			} else {
+				///////////////////////////////////////////////////////////////////
+				// S3 Code
+				///////////////////////////////////////////////////////////////////
+				// Instantiate the AmazonS3 class
+				$s3 = new AmazonS3(array("key"=>"AKIAINETGK3VTKANM25Q", "secret"=>"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"));
+				///////////////////////////////////////////////////////////////////
+				
 				$folderName = $_GET["devicetoken"];
 				$imageCategory = $_GET["imagetype"];
 				if(!is_dir($folderName)) {
@@ -20,7 +27,22 @@
 				$newfilename = $imageCategory."_".$newfilename.".jpeg";
 				move_uploaded_file($imageFile['tmp_name'],
 						$folderName."/".$newfilename);
-				echo "<response><code>100</code></response>";
+				
+				///////////////////////////////////////////////////////////////////
+				// S3 Code
+				///////////////////////////////////////////////////////////////////
+				$file = $folderName."/".$newfilename;
+				$bucket = "all-user-files";
+				$response = $s3->create_object($bucket, $file, array(
+						'fileUpload' => $file
+				));
+				///////////////////////////////////////////////////////////////////
+				
+				if ($response->isOk()){
+					echo "<response><code>100</code></response>";
+				}else{
+    				echo "<response><code>200</code></response>";
+				}
 			}
 		}
     } else {
