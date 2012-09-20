428555a03905bb7eb4c1626ea3a424e5d925c67c
fabricio.constantin@gmail.com <boquita29>
2012-09-19 22:57:34 -0300

    Add control to avoid assign the same key to multiple hotkeys

--- js/jviewer.js
+++ js/jviewer.js
@@ -448,6 +448,7 @@ JSViewer = function () {
                 if (e.keyCode == 13 || e.keyCode == 87 || e.keyCode == 81) {
                     document.getElementById(e.target.getAttribute('id')).value = "";
                     alert("This keys can't be assigned because it is a reserved key!");
+                    return false
                 }
                 for (var i = 1; i < rows.length; i++) {
                     if ( String.fromCharCode(e.keyCode) == rows[i].children[3].children[0].value
@@ -455,7 +456,7 @@ JSViewer = function () {
                         var name = rows[i].children[1].innerHTML;
                         document.getElementById(e.target.getAttribute('id')).value = "";
                         alert("This key can't be assigned because it is assigned to '" + name +"'!");
-                        return;
+                        return false;
                     }
                 }
                 
