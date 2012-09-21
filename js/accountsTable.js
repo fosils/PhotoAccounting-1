ca7360acf1e3b5b188a77a81a0b93f12fd1a0268
Fabricio Constantin <fabricio.constantin@orbitz.com>
2012-09-21 15:37:53 -0300

    Add error span for hotkey

--- js/accountsTable.js
+++ js/accountsTable.js
@@ -1,14 +1,17 @@
 YUI().use('datatable-scroll', "datasource-io", "datasource-jsonschema",
 		"datatable-datasource", function(Y) {
 			var columns = [ {
-				key : "Number",
-				label : "No."
-			}, 'Name', 'VAT', {
-				key : "Hotkey",
-				label : "HotKey",
-				allowHTML: true,
-				emptyCellValue: "<input type='text' maxlength='1' size='1' class='hotkey-input'/>"
-					."<span class='field_error'></span>" } ];
+					key : "Number",
+					label : "No."
+				},
+				'Name',
+				'VAT', {
+					key : "Hotkey",
+					label : "HotKey",
+					allowHTML: true,
+					emptyCellValue: "<input type='text' maxlength='1' size='1' class='hotkey-input'/><span class='field_error'></span>"
+				}
+			];
 
 			var dataSource = new Y.DataSource.IO({
 				source : "php/proxy.php?function=get_accounts"
