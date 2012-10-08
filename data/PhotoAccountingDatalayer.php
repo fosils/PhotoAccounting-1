c10ae4281a572cb21f6bd0142176b85fcffe0df8
igreactive <igreactive@gmail.com>
2012-10-08 15:19:38 +0200

    fix formatting

--- data/PhotoAccountingDatalayer.php
+++ data/PhotoAccountingDatalayer.php
@@ -17,22 +17,18 @@ class PhotoAccountingDatalyer extends PGDatalyer{
 	}
 	public function CUST_GetByID($id){
 		return $this->Exec("select * from customers where customer_id=:customer_id;", array(":customer_id"=>$id));
-	}
-	
-	
+	}	
 	public function RCT_Create($customer_id, $s3url){
 		return $this->Exec("insert into receipts(customer_id, s3url, received_date) values(:customer_id, :s3url, current_timestamp);", array(":customer_id"=>$customer_id, ":s3url"=>$s3url));
 	}
-
-	
 	public function CDV_Create($customer_id, $device_id){
 		return $this->Exec("insert into cust_devices(customer_id, udid) values(:customer_id, :device_id);", array(":customer_id"=>$customer_id, ":device_id"=>$device_id));
 	}
 	public function CDV_GetCustomerID($device_id){
 		return $this->Exec("select * from cust_devices where udid=:device_id limit 1;", array(":device_id"=>$device_id));
 	}
-        public function CDV_ExistsForCustomer($customer_id, $device_id){
-                return $this->Exec("select 1 as Exists from cust_devices where customer_id=:customer_id and udid=:device_id limit 1;", array(":customer_id"=>$customer_id, ":device_id"=>$device_id));
-        }
+    public function CDV_ExistsForCustomer($customer_id, $device_id){
+	    return $this->Exec("select 1 as Exists from cust_devices where customer_id=:customer_id and udid=:device_id limit 1;", array(":customer_id"=>$customer_id, ":device_id"=>$device_id));
+    }
 }
 ?>
