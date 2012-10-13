e049236cd5766f5c5325f160d3e88bf270c3d9b9
masterRules <ajakobczak@gmail.com>
2012-10-13 13:57:18 +0200

    deleted db.php

--- php/includes/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Connecting & selecting database
-$dbconn = pg_connect("host=localhost dbname=photo_accounting user=photo_editor password=Htbp4SAaxm6K")
-    or die('Could not connect: ' . pg_last_error());
-?>
